272d2a6e7af527b5cf3f012a4431fce655598655
Alireza Asadi <asadi.alireza@yahoo.com>
2019-10-30 22:15:32 +1100

    import com.applovin.sdk.AppLovinSdk

--- src/android/AdMobMediationApplovinPlugin.java
+++ src/android/AdMobMediationApplovinPlugin.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import com.applovin.sdk.AppLovinSdk;
 
 public class AdMobMediationApplovinPlugin extends CordovaPlugin {
 
